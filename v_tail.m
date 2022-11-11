564eb61edca95c6acd577dfe2a92614b554bea2a
Brachos <mathias.brach01@gmail.com>
2022-11-11 14:21:47 +0100

    Book method

--- v_tail.m
+++ v_tail.m
@@ -1,31 +1,77 @@
-%% Donnï¿½es
-AR= 7;
-SW = 10.76; %mï¿½
-bW = 10.8; %m
-c_root = 1.9; %m
-c_tip = 0.57; %m
-cVT_init = 0.06;  %between 0.02-0.09
-cHT_init = 0.8; %between 0.5-1
-LVT = 1; %vertical distance [m]
-LHT = 8; %horizontal distance [m]
+%Code destiné à obtenir les principaux paramètres géométrique de la tail en
+%fonction des caractéristiques des ailes. Cette méthode est basée sur
+%l'ouvrage de référence "Aircraft design, A Systems Engineering Approach"
+%de Mohammed H Sadraey et plus précisément du chapitre 6.
 
-%% Calculations
-fun = @(x) (c_root + (c_tip-c_root)/bW*2*x).^2;
-CW_bar = 2/SW * integral(fun,0,bW/2);
-SVT_init = cVT_init*bW*SW/LVT;
-SHT_init = cHT_init*CW_bar*SW/LHT;
-dihedral_angle_init = atan(sqrt(SVT_init/SHT_init));
-fprintf("L'angle initial dihedre est de %d degrés\n", dihedral_angle_init*180/2/pi)
-% choix a priori d'un profil NACA 0012
+%Cette méthode est itérative et s'appuie sur les équilibre statiques
+%et dynamique.
 
-%% Slide method
+%% Data required
+% - c_bar
+% - AR
+% - S
+% - ro
+% - h_o ~ [0.2;0.25]
+% - C_maf
+% - gamma
+% - alpha_t
+% - W_avg
+% - V_c
+% - D_f
 
-%Données
+%% Longitudinal trim
+% Comme présenté sur la figure 6.3 e l'ouvrage et dans l'équation 6.9, on a
+% l'équation d'équilibre suivante : M_owf + M_Lwf + M_Lh = 0 
+% où :
+% - M_owf est le 'wing/fuselage aerodynamic pitching moment'
+% - M_Lwf est le 'lift pitching moment of the wing/fuselage'
+% - M_Lh est le 'lift pitching moment of the tail'
 
+% En observant les caractéristiques géométrique de l'avion on peut réécrire
+% cette équation :  M_owf + L_wf*(h*c_bar - h_0*c_bar) - L_h*l_h = 0 (1)
+% (voir figure 6.3)
 
-%Calculs
-C_LT = (C_L - C_Lw)*Sw/ST;
-C_LT = a1*(C_Lw/a * (1-der_eps) + n_T + alpha_L0_root);
-C_Lw = (C_L0 - c_bbar/l_T * C_m0)/(1+(h-h0)*c_bbar/l_T);
-i_w = C_Lw_star/a + alpha01*eps_a_tip + alpha_l0_root;
-n_T = i_T - i_w;
+% Après les quelques étapes expliquées dans le livre, on tombe sur
+% l'équation : C_mo_wf + C_L*(h-h_o) - (l/c_bar * S_h/S) * C_Lh = 0 (2)
+
+% Le coefficient (l/c_bar * S_h/S)=:V_h_bar  est très important et est
+% appelé "horizontal tail volume coefficient". Il joue un rôle très
+% important dans la stabilité longitutinale de l'appareil. Après avoir
+% consulté plusieur sources statistiques, ce cefficient sera fixé à une
+% première valeur de 1;
+V_h_bar = 1;
+% Dans l'équation (1), on peut trouver les autres coefficients comme suit :
+C_mo_wf = C_maf*(AR*cos(gamma)^2)/(AR + 2*cos(gamma)) + 0.01*alpha_t;
+% où :
+% - C_maf est le 'wing airfoil section pitching moment coefficient'
+% - AR, l'aspect ratio des ailes
+% - gamma, le sweep angle des ailes
+% - alpha_t, le 'wing twist angle (in degrees)'
+C_L = 2*W_avg/(ro*V_c^2*S);
+% où :
+% - W_avg est le poids moyen pendant le 'cruising flight'
+% - V_c est la vitesse de croisière
+
+% L'équation (2) peut être rectifiée et réécrite en tenant compte de
+% l'efficacité de la tail
+% C_mo_wf + C_L*(h-h_o) - eta_h*V_h_bar*C_Lh = 0;
+%Nous choisirons une valeur d'efficacité moyenne de 0.9;
+eta_h = 0.9;
+% On prend également h = 0.25, valeur classique
+h = 0.25;
+%Admettons que h_o soit connu par l'étude de l'airfoyl, on peut donc
+%trouver C_Lh.
+C_Lh = (C_mo_wf + C_L*(h-h_o))/(eta_h*V_h_bar); %(3)
+% L'équation (3) est particulièrement importante dans le design de la tail
+% La longueur l (distance entre le centre aérodynamique de l'aile et le
+% centre aérdynamique de la tail) est correlé à la taille totale du
+% fuselage par le coefficient l/L. Encore une fois, via le tableau 6.2 du
+% livre, on donne l/L = 0.5
+% Cependant, connaissant le diamètre maximal du fuselage, S, c_bar et
+% V_h_bar, on peut déterminer la longueur l optimale.
+K_c = 1.1; % voir livre page 300)
+l = K_c * sqrt(4*c_bar*S*V_h_bar/(pi*D_f));
+% Ainsi, on peut maintenant détemriner S_h :
+S_h = V_h_bar*c_bar*S/l;
+% L'AR de la tail est déterminé selon la formule (6.59)
+AR_h = 2/3 * AR;
