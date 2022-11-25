1f68940179114970e2b5fe41d81e705696d06019
Flober19 <flo-muller@hotmail.com>
2022-11-25 14:31:46 +0100

    Aircraft

---
+++ Aircraft.m
@@ -0,0 +1,3 @@
+% Script that couple all codes together and determine if the aircraft is
+% stable or not
+
--- wing.m
+++ wing.m
@@ -1,5 +1,4 @@
-% function [b,S,CL,CD,D,c_root,c_tip,c_AC,x_AC,y_AC,V_fuel] = wing(Mach,Altitude,Mass,AR)
-% clear all;close all;clc;
+function [b,S,CL,CD,D,c_root,c_tip,c_AC,x_AC,y_AC,V_fuel] = wing(Mach,Altitude,Mass)
 
 %% Chosen airfoil: NASA SC(2)-0714  -> Optimal lift coefficient: cl = 0.7
 %                                   -> Design/divergence Mach #: MD = 0.725
@@ -11,14 +10,13 @@ curve   = [c_upper;c_lower(end:-1:1,:)];
 
 %% Data
 
-Mach     = 0.7;   % [-]
-Mass     = 5080;  % [kg]
-Altitude = 30000; % [feet]
-AR       = 7;     % [-]
+% Mach     = 0.7;   % [-]
+% Mass     = 5080;  % [kg]
+% Altitude = 30000; % [feet]
 
-g     = 9.81;    % [m/s²]
-W     = Mass*g;  % [N]     Take off weight (= Lift at cruise)
-L     = W;
+g  = 9.81;   % [m/s²]
+W  = Mass*g; % [N] Take off weight (= Lift at cruise)
+L  = W;
 
 [V_inf,rho] = speed(Altitude,Mach);     % 30 000 ft = 9 144 m 
 rho = 0.4671+(0.4135-0.4671)*0.144;     % [kg/m³]  Density at 30 000 ft
@@ -31,31 +29,30 @@ tap = 0.3; % Tapper ratio (lambda = c_tip/c_root) best value
 %% cl and AOA depending on airfoil profil (NASA SC(2)-0714)
 % Hypothesis: classical fuselage 
                                            
-AOA  = -0.5*pi/180; % AOA where the drag is minimum or cl/cd is maximum 
-cd   = 0.007;       % Minimum drag of this airfoil at Re = 1e6
-                    % !!! We have Re ~ 1e7
-cl   = 0.53;        % Airfoil lift coefficient with max(cl/cd) = 81.2 
+AOA = 0.75*pi/180; % AOA where the drag is minimum or cl/cd is maximum 
+cd  = 0.012;
+cl  = 0.7;
 
 %% Computation of wings properties
 % Sweep angle
 MD    = 0.725;
 MObj  = 0.9;
 sweep = acos(MD/MObj);
-beta    = sqrt(1-Mach^2);
-L_beta  = atan(tan(sweep)/beta); % Angle to graphically find x_AC
+beta   = sqrt(1-Mach^2);
+L_beta = atan(tan(sweep)/beta); % Angle to graphically find x_AC
 
 % CL and CD
-cl_alpha  = (0.9-0.3)*180/(pi*(1.5+2));
-alpha_l0  = -(0.8/cl_alpha - 1.5*pi/180);
+cl_alpha  = (0.76-0.27)*180/(pi*(1+2)); % [1/rad]
+alpha_l0  = -(0.83/cl_alpha - 1.5*pi/180);
 alpha_01  = -0.23;     % See L5 - P30
-theta_tip = 0;%-2*pi/180; % Twist angle
+theta_tip = -2*pi/180; % [rad] Twist angle
 alpha_L0  = alpha_l0 + alpha_01*theta_tip;
 k  = beta*cl_alpha/(2*pi);
 a  = 2*pi/(2/(beta*AR)+sqrt((1/(k*cos(L_beta)))^2+(2/(beta*AR))^2))/beta;
 
-CL = a*(AOA-alpha_L0)
-CL = 0.4;
-CD = 0.017+CL^2/(0.8*pi*AR);
+CL = a*(AOA-alpha_L0);
+% CD = 0.017+CL^2/(0.8*pi*AR);
+
 
 % Wing surface and span
 S  = 2*L/(rho*V_inf^2*CL);
@@ -84,16 +81,12 @@ A_root = trapz(c_root*curve(:,1),c_root*curve(:,2)); % [m²] Area of airfoil
 V_fuel = (A_root+A_tip)*b/2;                         % [m³] volume of wings
 
 %% Plot of airfoil profil
+% 
+% figure
+% plot(coord(1:103,1),coord(1:103,2),coord(104:end,1),coord(104:end,2))
+% plot(c_upper(:,1),c_upper(:,2),c_lower(:,1),c_lower(:,2))
+% xlabel('x/c [m]')
+% ylabel('z/c [m]')
+% axis equal
 
-figure
-plot(coord(1:103,1),coord(1:103,2),coord(104:end,1),coord(104:end,2))
-plot(c_upper(:,1),c_upper(:,2),c_lower(:,1),c_lower(:,2))
-xlabel('x/c [m]')
-ylabel('z/c [m]')
-axis equal
-
-%% Output
-
-sweep = sweep*180/pi;
-
-% end
+end
