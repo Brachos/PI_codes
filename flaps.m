27ff21f7a088ee8ad84cb0200176daf98cd0e88e
Flober19 <flo-muller@hotmail.com>
2023-04-24 21:28:03 +0200

    Auto stash before merge of "safety_code2" and "origin/safety_code2"

--- flaps.m
+++ flaps.m
@@ -14,7 +14,7 @@ function [CL_max_TO, CL_max_L, fb_ratio, S_flap] = flaps(WING,FUSELAGE)
     
     pente_lead  = dx_tip/(b/2);
     pente_trail = -(c_root-dx_tip-c_tip)/(b/2);
-    y_flap = [-R_fus-0.1 -R_fus/2-b/4];
+    y_flap = [-R_fus-0.1 -R_fus-0.1-b/2*0.4];
     xf1 = c_root + pente_lead*y_flap(1);
     xf2 = pente_trail*y_flap(1);
     xf3 = c_root + pente_lead*y_flap(2);
@@ -24,8 +24,8 @@ function [CL_max_TO, CL_max_L, fb_ratio, S_flap] = flaps(WING,FUSELAGE)
     fb_ratio = (y_flap(1)-y_flap(2))/(b/2);
     
     CL_max_base = 0.95*cos(sweep)*(1.75+1.55)/2*1.1; % Graph L5-S32
-    DCL_max_TO  = 0.9*cos(L_HL)*(S_flap/S)*0.95; % Graph L5-S33 with single slotted, Angle = 20 deg 
-    DCL_max_L   = 0.9*cos(L_HL)*(S_flap/S)*1.5;  % Angle = 40 deg 
+    DCL_max_TO  = 0.9*cos(L_HL)*(S_flap/S)*0.95 % Graph L5-S33 with single slotted, Angle = 20 deg 
+    DCL_max_L   = 0.9*cos(L_HL)*(S_flap/S)*1.5  % Angle = 40 deg 
     CL_max_TO = CL_max_base + DCL_max_TO;
     CL_max_L  = CL_max_base + DCL_max_L;
 end
